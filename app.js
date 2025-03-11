c68f3e22d77b591907dc70cce5a58ba4dc1fb6c3
emmanuel <emmanuel.carron@itscordoba.edu.ar>
2025-03-10 23:04:21 -0300

    Comentado el contexto de un msj

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowPrincipal = addKeyword(['hola', 'alo'])
-.addAnswer(['Hola, bienvenido a mi tienda', '¿Como puedo ayudarte?']) // cada string va en una linea nueva
-.addAnswer(['Tengo:', 'Zapatos', 'Bolsos', 'etc ...'])
+const flowPrincipal = addKeyword('Hola')
+.addAnswer('Bienvenido a este chatbot', {capture: true}, (ctx) => {
+    console.log('key: ' + Object.keys(ctx.key) +
+        ' pushName: ' + ctx.pushName + //nombre de perfil
+        ' broadcast: ' + ctx.broadcast + //boolean
+        ' message: ' + Object.keys(ctx.message) +
+        ' verifiedBizName: ' + ctx.verifiedBizName + //nombre de perfil
+        ' body: ' + ctx.body + //cuerpo del msj
+        ' from: ' + ctx.from); //num de telèfono
+})
 
 const main = async () => {
     const adapterDB = new MockAdapter()
